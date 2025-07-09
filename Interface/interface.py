660af035d1bec01f6edd4e32bfcce0323b99e0dc
Saskia <131782929+SaskiaJuergens@users.noreply.github.com>
2025-07-09 17:51:05 +0200

    Streamlit Interface erstellt

---
+++ Interface/interface.py
@@ -0,0 +1,36 @@
+import streamlit as st 
+
+def set_send_input():
+    st.session_state.send_input = True
+    clear_input_field()
+
+def clear_input_field():   
+    st.session_state.input_query = st.session_state.user_input #letzten Input speichern
+    st.session_state.user_input = "" #Input löschen
+
+def main():
+    st.title("KI-gestütze Threat Modeling")
+    chat_container = st.container()
+
+    #define send_input
+    if "send_input" not in st.session_state:
+        st.session_state.send_input = False
+        st.session_state.input_query = ""
+
+    user_input = st.text_input("Stelle eine Frage...", key="user_input", on_change=set_send_input)
+    send_button = st.button("Senden", key="send_button")
+
+    if send_button or st.session_state.send_input:   #Senden button betätigen
+        if st.session_state.input_query != "":
+
+            llm_response = "Anwort der LLM"
+
+            with chat_container:
+                #st.chat_message("user").write(user_input)
+                st.chat_message("user").write(st.session_state.input_query)
+                #st.chat_message("llm").write(llm_response)
+                st.chat_message("ai").write("AI Antwort!")
+        
+
+if __name__ == "__main__":
+    main()
